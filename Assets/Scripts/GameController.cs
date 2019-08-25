fc089783c145856e82a24387aae000759eea1f92
Santreg <54395370+Santreg@users.noreply.github.com>
2019-08-26 02:03:36 +0300

    part1

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+
+    [SerializeField]
+    public PlanetData earth;
+    public PlanetData moon;
+    public PlanetData jupiter;
+
+    public Transform menu;
+    public Text score;
+
+
+    public static GameController Instance;
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void StartEarthLevel()
+    {
+
+    }
+    void StartMoonLevel()
+    {
+
+    }
+    void StartJupiterLevel()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlanetData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "PData", menuName = "Planet Data", order = 51)]
+public class PlanetData : ScriptableObject
+{
+    [SerializeField]
+    Color color;
+    [SerializeField]
+    float gravity;
+}
