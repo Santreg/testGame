420369c0fc41439d501885d844da90665e6b1121
Santreg <54395370+Santreg@users.noreply.github.com>
2019-08-26 01:21:32 +0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rb;
+    public float force;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        rb.AddForce(Vector2.up*force, ForceMode2D.Impulse);
     }
 }
